f83ac5e160681fa5215b292a5e6a57c7bf3120ba
tiagodelima-sc <tiago.l07@aluno.ifsc.edu.br>
2022-02-07 22:56:08 -0300

    Modelos criados

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,72 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Genero(models.Model):
+  
+    nome = models.CharField(max_length=200, help_text='Entre com um genero de livro.')
+    
+    def __str__(self):
+        return self.nome
+      
+class Linguaguem(models.Model):
+  
+    nome = models.CharField(max_length=200, help_text='Entre com a Linguagyem do Livro.')
+    
+    def __str__(self):
+        return self.nome
+      
+class Autor(models.Model):
+  
+      primeiro_nome = models.CharField(max_length=100)
+      ultimo_nome = models.CharField(max_length=100)
+      data_nascimento = models.DateField(null=True, blank=True)
+      data_falecimento = models.DateField(null=True, blank=True)
+      
+      class Meta:
+        ordering = ['ultimo_nome', 'primeiro_nome']
+        
+      def __str__(self):
+        return f'{self.ultimo_nome}, {self.primeiro_nome}'
+      
+class Livro(models.Model):
+  
+      titulo = models.CharField(max_length=200)
+      autor = models.ForeignKey(Autor, on_delete=models.SET_NULL, null=True)
+      resumo = models.TextField(max_length=1000, help_text='Digite uam descrição do livro')
+      isbn = models.ManyToManyField(Genero, help_text='Selecione um gênero para esse livro.')
+      linguagem = models.ForeignKey(Linguaguem, on_delete=models.SET_NULL, null=True)
+      
+      def __str__(self):
+        return self.titulo
+      
+import uuid
+
+class ExemplarLivro(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Identificar único para esse exemplar')
+  livro = models.ForeignKey(Livro, on_delete=models.SET_NULL, null=True)
+  editora = models.CharField(max_length=200)
+  data_devolucao = models.DateField(null=True, blank=True)
+  
+  SITUACAO_EXEMPLAR = (
+    ('m', 'Manutenção'),
+    ('e', 'Emprestado'),
+    ('d', 'Disponivel'),
+    ('r', 'Reservado'),
+  )
+  
+  situacao = models.CharField(
+    max_length=1,
+    choices=SITUACAO_EXEMPLAR,
+    blank=True,
+    default='m',
+    help_text='Situação do Exemplar'
+  )
+  
+  class Meta:
+    ordering = ['data_devolucao']
+    
+  def __str__(self):
+    return f'{self.id} ({self.livro})'
+      
