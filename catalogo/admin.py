212ec1dbef7c767e93c254d7fe97762ec4f8f0dc
tiagodelima-sc <tiago.l07@aluno.ifsc.edu.br>
2022-02-08 19:28:39 -0300

    modelos registrado no painel adm, fixture

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalogo.models import Genero, Linguaguem, Autor, Livro, ExemplarLivro
+
+admin.site.register(Genero)
+admin.site.register(Linguaguem)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(ExemplarLivro)
