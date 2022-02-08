9e5dbb7ea51d8a769bda39c0352d7de2210b79b2
tiagodelima-sc <tiago.l07@aluno.ifsc.edu.br>
2022-02-08 20:06:25 -0300

    página inicial biblioteca

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,7 +1,8 @@
 import imp
+from unicodedata import name
 from django.urls import path
 from catalogo import views
 
 urlpatterns = [
-  
+  path('', views.index, name='index')
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from catalogo.models import Genero, Linguaguem, Autor, Livro, ExemplarLivro
+
+def index(request):
+  
+    # contando o numero de livros e exemplares  
+    num_livros = Livro.objects.all().count()
+    num_exemplares = ExemplarLivro.objects.all().count()
+    
+    # Contando a quantidade de exemplares disponiveis (situacao = 'd')
+    num_exemplares_disponiveis = ExemplarLivro.objects.filter(situacao__exact='d').count()
+    
+    # Contando o numero de autores. A opcao 'all()' é implicita por padrao
+    num_autores = Autor.objects.count()
+    
+    contexto = {
+      'num_livros': num_livros,
+      'num_exemplares': num_exemplares,
+      'num_exemplares_disponiveis': num_exemplares_disponiveis,
+      'num_autores': num_autores,
+    }
+    
+    # renderizando o template index.html com os dados da variavel contexto
+    return render(request, 'index.html', context=contexto)
+  
+    
