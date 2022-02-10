063c6617b34026491b990ce438fa4c15e5f14299
tiagodelima-sc <tiago.l07@aluno.ifsc.edu.br>
2022-02-09 23:50:12 -0300

    heroku config

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%g(0+8h8_%w3_c!=svlph(f7*w^fgon72=^jt$z#2j)8bawf!'
+# SECRET_KEY = 'django-insecure-4%g(0+8h8_%w3_c!=svlph(f7*w^fgon72=^jt$z#2j)8bawf!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)bkl#$(4iy!n%mia*1k_ff93qx413b(=$$h-rec!boi7lvvqt6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,6 +84,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
